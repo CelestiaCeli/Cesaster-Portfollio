a86faa46fb9a01bdbd5e6d3ec5adbf0544bb7ea0
CelestiaCeli <Celesteisabella@proton.me>
2026-07-30 08:55:21 -0500

    Working on my website for a couple days

--- Webpages/JavaScript/Objects/Fader.js
+++ Webpages/JavaScript/Objects/Fader.js
@@ -23,69 +23,69 @@ class Fader
 		}
 	}
 
-function SetFadeAmt()
-{
-	this.lowFadeAmt = _lowFadeAmt;
-	this.highFadeAmt = _highFadeAmt;
-}
-
-function GenerateFadeAnim(element)
-{
-	if (this.lowFadeAmt == null || highFadeAmt == null)
+	function SetFadeAmt()
 	{
-		GenerateDefaultFadeAnim(element);
+		this.lowFadeAmt = _lowFadeAmt;
+		this.highFadeAmt = _highFadeAmt;
 	}
-	else
+
+	function GenerateFadeAnim(element)
 	{
-		GenerateCustomFadeAnim(element);
+		if (this.lowFadeAmt == null || highFadeAmt == null)
+		{
+			GenerateDefaultFadeAnim(element);
+		}
+		else
+		{
+			GenerateCustomFadeAnim(element);
+		}
 	}
-}
 
-function GenerateCustomFadeAnim(element)
-{
-	var fadeInAnim = [ { opacity: lowFadeAmt + "%" }, { opacity: highFadeAmt + "%" } ];
-	var fadeOutAnim = [ { opacity: highFadeAmt + "%" }, { opacity: lowFadeAmt + "%" } ];
-	var titleFadeSpeed = { duration: 500, iterations: 1, complete: JustifyOpacity(element, switched), }
-
-	if (fadeIn = true)
+	function GenerateCustomFadeAnim(element)
 	{
-		return (fadeInAnim, titleFadeSpeed);
+		var fadeInAnim = [ { opacity: lowFadeAmt + "%" }, { opacity: highFadeAmt + "%" } ];
+		var fadeOutAnim = [ { opacity: highFadeAmt + "%" }, { opacity: lowFadeAmt + "%" } ];
+		var titleFadeSpeed = { duration: 500, iterations: 1, complete: JustifyOpacity(element, switched), }
+
+		if (fadeIn = true)
+		{
+			return (fadeInAnim, titleFadeSpeed);
+		}
+		else
+		{
+			return (fadeOutAnim, titleFadeSpeed);
+		}
 	}
-	else
+
+	function GenerateDefaultFadeAnim()
 	{
-		return (fadeOutAnim, titleFadeSpeed);
+		var fadeInAnim = [ { opacity: DEFAULT_FADEOUT + "%" }, { opacity: DEFAULT_FADEIN + "%" } ];
+		var fadeOutAnim = [ { opacity: DEFAULT_FADEIN + "%" }, { opacity: DEFAULT_FADEOUT + "%" } ];
+		var titleFadeSpeed = { duration: 500, iterations: 1, complete: JustifyOpacity(element, switched), }
+
+		if (fadeIn = true)
+		{
+			return (fadeInAnim, titleFadeSpeed);
+		}
+		else
+		{
+			return (fadeOutAnim, titleFadeSpeed);
+		}
 	}
-}
 
-function GenerateDefaultFadeAnim()
-{
-	var fadeInAnim = [ { opacity: DEFAULT_FADEOUT + "%" }, { opacity: DEFAULT_FADEIN + "%" } ];
-	var fadeOutAnim = [ { opacity: DEFAULT_FADEIN + "%" }, { opacity: DEFAULT_FADEOUT + "%" } ];
-	var titleFadeSpeed = { duration: 500, iterations: 1, complete: JustifyOpacity(element, switched), }
 
-	if (fadeIn = true)
+	function SetOpacity(_lowFadeAmt, _highFadeAmt)
 	{
-		return (fadeInAnim, titleFadeSpeed);
+		lowFadeAmt = _lowFadeAmt;
+		highFadeAmt = _highFadeAmt;
+
 	}
-	else
+
+	function BeginAnimation(element)
 	{
-		return (fadeOutAnim, titleFadeSpeed);
+		element.animate(element);
+
 	}
-}
-
-
-function SetOpacity(_lowFadeAmt, _highFadeAmt)
-{
-	lowFadeAmt = _lowFadeAmt;
-	highFadeAmt = _highFadeAmt;
-
-}
-
-function BeginAnimation(element)
-{
-	element.animate(element);
-
-}
 	
 
 
